4d591498487e32b9064b6fb4cf187aff93600606
fufan <fufan@fufan-MacBook-Pro.local>
2019-07-17 21:04:12 +0800

    implement a simple LinkedList

---
+++ src/main/java/com/cloud/gemini/leetcode/medium/LengthOfLongestSubstring.java
@@ -0,0 +1,12 @@
+package com.cloud.gemini.leetcode.medium;
+
+/**
+ * created by fufan on 2019-07-17 10:04
+ **/
+public class LengthOfLongestSubstring {
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        return 0;
+    }
+}
